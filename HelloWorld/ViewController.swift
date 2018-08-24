26056125b1ad9ff07269f5207f9e6fca9c0d7079
Sharoze Amir <sharozeamir@Sharozes-MacBook-Pro.local>
2018-08-24 18:09:13 -0500

    Completed Challenge

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var lab: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,18 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func helloClicked(){
+        
+        lab.text = "Hello World"
+        
+    }
+    
+    @IBAction func clearClicked(){
+        
+        lab.text = ""
+        
     }
 
 
